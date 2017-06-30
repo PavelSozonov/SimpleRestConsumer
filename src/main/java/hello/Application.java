db6b897cb55ad280c310098c0df2ab5004253169
Pavel Sozonov <pavel.sozonov@gmail.com>
2017-07-01 01:26:34 +0300

    Simple Rest Consumer

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by pavel on 30.06.17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
